d971c4bbe8b204c3c545f519fd80a9026d50e6c6
Guansong Wang <wang.guansong@hotmail.com>
2025-02-27 23:16:27 +0800

    referrer policy

--- cc/settings.py
+++ cc/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+SECURE_REFERRER_POLICY = 'strict-origin-when-cross-origin'
